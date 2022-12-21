5bdbeadfee876f31461b5002ebad0e68076abf26
dam <m.maeda@newbees.jp>
2022-12-21 15:51:26 +0900

    feat:環境変数の読み込みの調整。

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseClient = createClient(
-  VITE_VERCEL_SUPABASE_URL,
-  VITE_VERCEL_SUPABASE_ANON_KEY
+  process.env.VITE_VERCEL_SUPABASE_URL,
+  process.env.VITE_VERCEL_SUPABASE_ANON_KEY
 );
 
 export default supabaseClient;
