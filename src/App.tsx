8ffe682907b8fa6fbfd7e0abe195cdda7e4de4c4
dam <m.maeda@newbees.jp>
2022-12-21 15:58:36 +0900

    feat:vercelの環境変数が取得できるかテスト。

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import TodoList from "./components/parts/TodoList";
 import styles from "./App.module.css";
 
 const App: Component = () => {
+  console.log(VITE_VERCEL_URL);
+  console.log(VITE_VERCEL_ENV);
   return (
     <>
       <Header />
       <section class="container"></section>
       <h2 class="title">Todo一覧</h2>
       <TodoForm />
-      <TodoList />
+      {/* <TodoList /> */}
     </>
   );
 };
